7a7f203cb5b16dcb6a4c228e030be25096bbf322
Jose Mari Escalante <jmescalante@up.edu.ph>
2019-05-05 22:03:45 +0800

    Add add-voter form validation.

--- client/src/components/customized/dialogs/add-voter.js
+++ client/src/components/customized/dialogs/add-voter.js
@@ -14,6 +14,7 @@ import Grid from '@material-ui/core/Grid'
 import CancelButton from '../buttons/cancel'
 import SubmitButton from '../buttons/submit'
 import CustomizedTextField from '../forms/textfield'
+import FormValidator from '../forms/form-validator'
 
 import { addVoterVotechain } from '../../../actions/voter'
 
@@ -43,54 +44,118 @@ class AddVoterDialog extends React.Component {
   constructor(props) {
     super(props)
 
-
     this.state = {
-      voterName: '',
-      studentNo: '',
-      voterKey: '',
+      fields: {},
+      errors: {},
     }
 
-    this.onChangeVoterName = this.onChangeVoterName.bind(this)
-    this.onChangeStudentNo = this.onChangeStudentNo.bind(this)
-    this.onChangeVoterKey = this.onChangeVoterKey.bind(this)
+    this.onEntered = this.onEntered.bind(this)
+    this.refreshErrorState = this.refreshErrorState.bind(this)
+    this.refreshFieldState = this.refreshFieldState.bind(this)
+
+    this.handleFieldChange = this.handleFieldChange.bind(this)
+    this.validateInputs = this.validateInputs.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
+
   }
 
-  onChangeVoterName(event) {
-    this.setState({ voterName: event.target.value })
+  async onEntered() {
+    await this.refreshErrorState()
+    await this.refreshFieldState()
   }
 
-  onChangeStudentNo(event) {
-    this.setState({ studentNo: event.target.value })
+  async refreshErrorState() {
+    await this.setState({ errors: {} })
   }
 
-  onChangeVoterKey(event) {
-    this.setState({ voterKey: event.target.value })
+  async refreshFieldState() {
+    await this.setState({ fields: {} })
+  }
+  
+  handleFieldChange(field, value) {
+    let { fields } = this.state
+    fields[field] = value
+    this.setState( fields )
   }
 
-  onSubmit() {
+  async validateInputs(){
+    const { fields } = this.state
+    const { web3, votechain, electionId } = this.props
+    await this.refreshErrorState()
+
+    let { errors } = this.state
+
+    let noOfErrors = 0
+
+    let voterKey = fields['voterKey']
+
+    if(!web3.utils.isAddress(voterKey)){
+      errors['voterKey'] = 'Invalid account address'
+      noOfErrors++
+    } else if ((await votechain.methods.isVoterAt(electionId, voterKey).call())) {
+      errors['voterKey'] = 'Already registered as a voter in this election'
+      noOfErrors++;
+    } else if ((await votechain.methods.isOfficial(voterKey).call())) {
+      errors['voterKey'] = 'Already registered as an official'
+      noOfErrors++;
+    } else if ((await votechain.methods.isAdmin(voterKey).call())) {
+      errors['voterKey'] = 'Already registered as an admin'
+      noOfErrors++;
+    }
+
+    let voterName = fields['voterName']
+
+    if(FormValidator.isEmpty(voterName)){
+      errors['voterName'] = 'The voter name must not be empty'
+      noOfErrors++
+    } else if (!FormValidator.validLength(voterName, 1, 32)) {
+      errors['voterName'] = 'The voter name must contain 1 - 32 characters only'
+      noOfErrors++
+    }
+ 
+    let studentNo = fields['studentNo']
+
+    if(FormValidator.isEmpty(studentNo)){
+      errors['studentNo'] = 'The student no must not be empty'
+      noOfErrors++
+    } else if (!FormValidator.validLength(studentNo, 1, 32)) {
+      errors['studentNo'] = 'The student no must contain 1 - 32 characters only'
+      noOfErrors++
+    }   
+    this.setState({ errors })
+
+    return noOfErrors
+  }
+
+  async onSubmit() {
     const { handleClickCloseDialog, addVoterVotechain, votechain, account, electionId } = this.props
-    const { voterName, studentNo, voterKey } = this.state
+    const { fields } = this.state
 
     let voter = {
       electionKey: electionId,
-      voterKey,
-      studentNo,
-      name: voterName,
+      voterKey: fields['voterKey'],
+      studentNo: fields['studentNo'],
+      name: fields['voterName'],
     }
 
-    addVoterVotechain(account, votechain, voter)
-    handleClickCloseDialog()
+    let noOfErrors = await this.validateInputs()
+
+    if(noOfErrors === 0){
+      addVoterVotechain(account, votechain, voter)
+      handleClickCloseDialog()
+    }
   }
   
 
   render() {
     const { classes, openDialog, handleClickCloseDialog } = this.props
+    const { errors } = this.state
 
     return (
       <Dialog
         open={openDialog}
         onClose={handleClickCloseDialog}
+        onEntered={this.onEntered}
       >
         <DialogTitle disableTypography>
           <Typography className={classes.label}>Add New Voter</Typography>
@@ -107,11 +172,12 @@ class AddVoterDialog extends React.Component {
             }}
             required
             fullWidth
-            type='string'
+            type='text'
             id='voter-key'
             label='Voter Key/Address'
             variant='outlined'
-            onChange={this.onChangeVoterKey}
+            onChange={(event) => this.handleFieldChange('voterKey', event.target.value)}
+            error={errors['voterKey']}
           />
 
           <CustomizedTextField
@@ -120,11 +186,12 @@ class AddVoterDialog extends React.Component {
             }}
             required
             fullWidth
-            type='string'
+            type='text'
             id='voter-name'
             label='Voter Name'
             variant='outlined'
-            onChange={this.onChangeVoterName}
+            onChange={(event) => this.handleFieldChange('voterName', event.target.value)}
+            error={errors['voterName']}
           />
 
           <CustomizedTextField
@@ -133,11 +200,12 @@ class AddVoterDialog extends React.Component {
             }}
             required
             fullWidth
-            type='string'
+            type='text'
             id='student-no'
             label="Voter's student number"
             variant='outlined'
-            onChange={this.onChangeStudentNo}
+            onChange={(event) => this.handleFieldChange('studentNo', event.target.value)}
+            error={errors['studentNo']}
           />
         </DialogContent>
 
@@ -168,6 +236,7 @@ AddVoterDialog.propTypes = {
 const mapStateToProps = state => ({
   account: state.account.account,
   votechain: state.contract.votechain,
+  web3: state.web3.web3,
 })
 
 const mapDispatchToProps = {
