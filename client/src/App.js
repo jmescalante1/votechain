6e91a8ec2abeb844930cdbd1b53be9309d523bd7
Jose Mari Escalante <jmescalante@up.edu.ph>
2019-05-07 23:13:20 +0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -267,7 +267,7 @@ class App extends React.Component {
   render() {
     const { loading, noAccounts } = this.state
     const { web3Error } = this.props
-
+    console.log('test')
     return (
       <Fragment>
         {loading
